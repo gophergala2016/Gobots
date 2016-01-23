b52d0ae8ea42d670231a8925653394b50a0b66df
Ross Light <ross@zombiezen.com>
2016-01-22 20:43:26 -0800

    db: sketch out interface

--- db.go
+++ db.go
@@ -1,4 +1,34 @@
 package main
 
+import "github.com/gophergala2016/Gobots/botapi"
+
 type db interface {
+	// Users
+	createUser( /* user ID/creds */ ) error
+
+	// AIs
+	createAI(u userID, nick string) (id aiID, token string, err error)
+	listAIsForUser(u userID) ([]*aiInfo, error)
+	lookupAI(id aiID) (*aiInfo, error)
+	lookupAIToken(token string) (*aiInfo, error)
+
+	// Games
+	startGame(ai1, ai2 aiID) (gameID, botapi.Board, error)
+	addRound(id gameID, round botapi.Replay_Round) error
+	lookupGame(id gameID) (botapi.Replay, error)
+}
+
+type userID string
+
+type aiID string
+
+type gameID string
+
+type aiInfo struct {
+	id    aiID
+	nick  string
+	token string
+
+	wins   int
+	losses int
 }
